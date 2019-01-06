b24bd5090303f8d16594c8cf18bb7bd3af71bcb6
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2019-01-06 22:25:13 +0600

    Handlebars Setup For Template Engine & Show All Gigs Data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const exphb = require('express-handlebars')
+const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const path = require('path');
 
 //Database
 const db = require('./config/database')
@@ -12,14 +13,25 @@ db.authenticate()
 
 const app = express();
 
+//HandleBars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname,'public')));
+
 //Gig Routes
 app.use('/gigs',require('./routes/gigs'));
 
 
 const PORT = process.env.PORT || 5000
 
+//Index Route
 app.get('/',(req,res) => {
-	res.send('Hello');
-})
+	// res.send('Hello');
+	res.render('index',{
+		layout: 'landing'
+	});
+});
 
 app.listen(PORT,console.log(`Server Started on ${PORT}`))
--- routes/gigs.js
+++ routes/gigs.js
@@ -5,14 +5,47 @@ const db = require('../config/database')
 //Models
 const Gig = require('../models/Gig')
 
+//Get Gig List
 router.get('/',(req,res) => {
 	// res.send('Gigs')
 	Gig.findAll()
 	   .then(gigs => {
-	   	console.log(gigs);
-	   	res.sendStatus(200);
+	   	// console.log(gigs);
+	   	// res.sendStatus(200);
+	   	res.render('gigs',{
+			gigs
+	   	});
 	   })
 	   .catch(err => console.log(err))
+});
+
+//Display Add ig Form
+router.get('/add',(req,res) => {
+	res.render('add');
+});
+
+//Add a Gig
+router.post('/add',(req,res) => {
+	const data = {
+		title: 'My Title',
+		technologies: 'PHP,Python,JavaScript,NodeKs',
+		budget: '$3600',
+		description: 'My Description',
+		contact_email: 'my@gmail.com'
+	}
+
+	let { title,technologies,budget,description,contact_email } =  data;
+
+	//Insert Into Tabel
+	Gig.create({
+		title,
+		technologies,
+		budget,
+		description,
+		contact_email
+	})
+	.then(gig => res.redirect('/gigs'))
+	.catch(err => console.log(err));
 });
 
 module.exports = router;
